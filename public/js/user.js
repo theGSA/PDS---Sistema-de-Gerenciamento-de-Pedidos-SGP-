d293026b6b944fa00c6e60e8a94dff8bb72e36b1
jvinicius7 <151785036+jvinicius7@users.noreply.github.com>
2023-11-29 17:04:59 -0300

    Validação de telefone
    Validação do número de telefone na tela de cadastro de usuário

--- public/js/user.js
+++ public/js/user.js
@@ -70,3 +70,17 @@ function RefreshImage(element)
         reader.readAsDataURL(file);
     }
 }
+
+//valida o número de telefone na tela de cadastro
+const handlePhone = (event) => {
+    let input = event.target
+    input.value = phoneMask(input.value)
+  }
+  
+  const phoneMask = (value) => {
+    if (!value) return ""
+    value = value.replace(/\D/g,'')
+    value = value.replace(/(\d{2})(\d)/,"($1) $2")
+    value = value.replace(/(\d)(\d{4})$/,"$1-$2")
+    return value
+  }
